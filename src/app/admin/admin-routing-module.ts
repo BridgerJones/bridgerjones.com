780c99f7abbbec55287d7a091537adf81b34e6bb
Bridger Jones <bridgerjones15@gmail.com>
2022-10-17 14:19:50 -0500

    adding authentication and routing with auth guard

--- src/app/admin/admin-routing-module.ts
+++ src/app/admin/admin-routing-module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminMainComponent } from './admin-main/admin-main.component';
 import { AuthComponent } from './auth/auth.component';
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['/sudo']);
+const redirectLoggedInToItems = () => redirectLoggedInTo(['/sudo/admin']);
 
 const routes: Routes = [
-  {path: '', component: AuthComponent},
+  {path: '', component: AuthComponent, canActivate: [AngularFireAuthGuard], data: {authGuardPipe: redirectLoggedInToItems}},
+  {path: 'admin', component: AdminMainComponent, canActivate: [AngularFireAuthGuard], data: {authGuardPipe: redirectUnauthorizedToLogin}},
 ];
 
 @NgModule({
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminMainComponent } from './admin-main/admin-main.component';
 import { AuthComponent } from './auth/auth.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin-routing-module';
 import { CheckboxModule } from 'primeng/checkbox';
 import { StyleClassModule } from 'primeng/styleclass';
@@ -18,6 +18,7 @@ import { StyleClassModule } from 'primeng/styleclass';
     CommonModule,
     AdminRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     CheckboxModule,
     StyleClassModule
   ]
